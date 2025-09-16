822f934569603f4663e2d5695e7ffecbac8ab844
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-16 16:15:36 +0300

    upgrade

--- services/infographic-converter/vite.config.ts
+++ services/infographic-converter/vite.config.ts
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-  host: true,
-  watch: {
-    usePolling: true
-  },
-  allowedHosts: ["infographic-service.onrender.com"],
-  port: 5173, 
-  cors: true 
-}
-
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      watch: {
+        usePolling: true
+      },
+      allowedHosts: ["infographic-service.onrender.com"],
+      cors: true,
+      port: 5173
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.')
+      }
+    }
+  };
+});
