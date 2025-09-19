59284b1a7b17a39d6842ed2db9c3db7158abec8a
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-19 13:43:57 +0300

    Update views.py

--- backend/style_editor_data/views.py
+++ backend/style_editor_data/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
 from .models import StyleExample
 from .serializers import StyleExampleSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class StyleExampleViewSet(viewsets.ModelViewSet):
     queryset = StyleExample.objects.all()
     serializer_class = StyleExampleSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
