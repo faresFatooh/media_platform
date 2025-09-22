664d77bd197727c1e372cd7283283c1a9564da8b
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-22 22:48:15 +0300

    Update vite.config.ts

--- services/discovery-script-service/vite.config.ts
+++ services/discovery-script-service/vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // هذا الإعداد ضروري ليعمل الخادم داخل حاوية Docker
-    watch: {
-      usePolling: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()], 
+    
+    server: {
+      host: true, 
+      watch: {
+        usePolling: true 
+      },
+      allowedHosts: ['.onrender.com']
     },
-    hmr: {
-      // هذا يخبر عميل Vite بكيفية الاتصال بشكل صحيح
-      // عندما يعمل خلف وكيل Render الذي يستخدم HTTPS على منفذ 443
-      clientPort: 443,
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
     }
-  }
-})
+  };
+});
