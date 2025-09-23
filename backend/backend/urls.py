bd245f9279acd5cb385dc456f8fdb6f83f461963
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-23 05:17:06 +0300

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('api/style-examples/', include('style_editor_data.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("health", health_check),
+    path("health", lambda request: JsonResponse({"status": "ok"})),  # مباشر
     path('api/asharq-automation/', include('asharq_automation.urls')),
     path("api/discovery-script/configs/", ApiConfigView.as_view(), name="api-configs"),
