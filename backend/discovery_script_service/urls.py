16e7a0eafc24355c41331a49ed7d9bd64270d889
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-23 05:04:15 +0300

    Update urls.py

--- backend/discovery_script_service/urls.py
+++ backend/discovery_script_service/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ApiConfigView
 
 urlpatterns = [
-    path("api/configs/", ApiConfigView.as_view(), name="api-configs"),
+    path("configs/", ApiConfigView.as_view(), name="api-configs"),
 ]
