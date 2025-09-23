71d58862829ac3678cf3ea647e2cdd3a7701bea9
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-23 05:53:57 +0300

    fars

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/asharq-automation/', include('asharq_automation.urls')),
-    path("api/discovery-script/configs/", ApiConfigView.as_view(), name="api-configs"),
+    path("discovery-script/api/configs/", include("discovery_script_service.urls"))
     path("health", lambda request: JsonResponse({"status": "ok"})),  # ✅ health check
 
 ]
