57b70f1b108a0afb59ba8433108db67da65d4019
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-25 23:23:29 +0300

    Update views.py

--- backend/news_generator/views.py
+++ backend/news_generator/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import EditorialStyle, CustomNewsSource, MonitoredSource
