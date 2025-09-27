c853d7393ef6fdf647eb3646a27d3d8e7b537256
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-28 00:38:55 +0300

    Update [id].js

--- frontend/pages/app/[id].js
+++ frontend/pages/app/[id].js
@@ -13,8 +13,8 @@ export default function AppDetails() {
 
   const services = {
     '1': { url: 'https://ai-news-generator-service.onrender.com', name:'مولد-المقالات-بالذكاء-الاصطناعي' },
-    '2': { url: 'https://ai-news-app-lpgh.onrender.com/', name:'مولد-الأخبار-بالذكاء-الاصطناعي' },
-    '3': { url: 'https://ai-news-app-lpgh.onrender.com/', name:'مُحَوِّل-النصوص-إلى-انفوجرافيك' },
+    '2': { url: 'https://ai-news-app-lpgh.onrender.com', name:'مولد-الأخبار-بالذكاء-الاصطناعي' },
+    '3': { url: 'https://infographic-converter.onrender.com', name:'مُحَوِّل-النصوص-إلى-انفوجرافيك' },
 
 
   };
