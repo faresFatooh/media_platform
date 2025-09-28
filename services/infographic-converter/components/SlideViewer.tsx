f305a99e35cd1b8388cdde903985e3b0c45476e2
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-09-28 22:29:01 +0300

    Update SlideViewer.tsx

--- services/infographic-converter/components/SlideViewer.tsx
+++ services/infographic-converter/components/SlideViewer.tsx
@@ -139,8 +139,7 @@ export const SlideViewer: React.FC<SlideViewerProps> = ({ slides, onSlideUpdate,
              if(originalButton) originalButton.textContent = 'تصدير PNG';
         }
     };
-
-
+    
     const exportAsPdf = async () => {
         setIsExporting(true);
         try {
