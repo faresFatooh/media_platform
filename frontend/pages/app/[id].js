1315c586898b45392cfa8c936e0cd97fb93cc2d7
Fares Fattouh <85830021+faresFatooh@users.noreply.github.com>
2025-10-15 17:02:06 +0300

    Update [id].js

--- frontend/pages/app/[id].js
+++ frontend/pages/app/[id].js
@@ -18,6 +18,7 @@ export default function AppDetails() {
     '4': { url: 'https://pal-content-generator.onrender.com', name:'تطبيق مولد الاخبار الفلسطينية' },
     '5': { url: 'https://youtube-documentary-script-generator.onrender.com', name:'تطبيق مولد اليوتيوب ' },
     '6': { url: 'https://ai-news-content-generator.onrender.com', name:'مولد المحتوى الاخباري' },
+    '7': { url: 'https://media-center-automation-dashboard.onrender.com', name:'نظام أتمتة مركز الإعلام' },
 
 
   };
